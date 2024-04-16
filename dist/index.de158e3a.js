7a81a5ce0c934f18ccd06285942becd9e3bcaea9
ignasimgol <ignasimgol@gmail.com>
2024-04-16 15:54:15 +0200

    media querys and hamburger menu done

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,3 +1,14 @@
+// Hamburger menu
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+});
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+});
+// Contador
 document.addEventListener("DOMContentLoaded", ()=>{
     // Obtener la fecha y hora objetivo: 03:14:07 UTC del 19 de enero de 2038
     const targetDate = new Date(Date.UTC(2038, 0, 19, 3, 14, 7));
@@ -10,8 +21,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const ver = document.getElementById("ver");
     ver.innerHTML = flipdown.version;
 });
+// Boton atras
 document.getElementById("btnBack").addEventListener("click", function() {
-    // Retrocede a la página anterior en la historia del navegador
     history.back();
 });
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+
+// Hamburger menu
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+// Contador
+
 document.addEventListener('DOMContentLoaded', () => {
     // Obtener la fecha y hora objetivo: 03:14:07 UTC del 19 de enero de 2038
     const targetDate = new Date(Date.UTC(2038, 0, 19, 3, 14, 7));
@@ -15,8 +32,9 @@ document.addEventListener('DOMContentLoaded', () => {
     ver.innerHTML = flipdown.version;
 });
 
+// Boton atras
 
 document.getElementById("btnBack").addEventListener("click", function() {
-  // Retrocede a la página anterior en la historia del navegador
   history.back();
 });
+
