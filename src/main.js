9c153c144bd6d9aa09822d822e7fa9b0327c46ef
ignasimgol <ignasimgol@gmail.com>
2024-04-18 10:44:17 +0200

    last changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 
-
 // Hamburger menu
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
@@ -14,32 +13,34 @@ cerrar.addEventListener("click", () => {
 })
 
 // Contador
-const fechaObjetivo = new Date('2038-01-19T03:14:07Z').getTime();
+const fechaObjetivo = moment('2038-01-19T03:14:07Z');
 
-// Actualiza el contador cada segundo
 const intervalo = setInterval(() => {
-  // Obtén la fecha y hora actual en milisegundos
-  const ahora = new Date().getTime();
-
-  const diferencia = fechaObjetivo - ahora;
   
+  const ahora = moment();
 
-  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
   
+  const diferencia = fechaObjetivo.diff(ahora);
 
+
+  const dias = fechaObjetivo.diff(ahora, 'days');
+  const horas = fechaObjetivo.diff(ahora, 'hours') % 24;
+  const minutos = fechaObjetivo.diff(ahora, 'minutes') % 60;
+  const segundos = fechaObjetivo.diff(ahora, 'seconds') % 60;
+
+  
   document.getElementById('dias').innerHTML = `${dias} días`;
   document.getElementById('horas').innerHTML = `${horas} horas`;
   document.getElementById('minutos').innerHTML = `${minutos} minutos`;
   document.getElementById('segundos').innerHTML = `${segundos} segundos`;
 
+
   if (diferencia < 0) {
     clearInterval(intervalo);
     document.getElementById('contador').innerHTML = 'TIME IS OVER';
   }
-}, 1000); // Se ejecuta cada segundo
+}, 1000);
+
 
 
 // Boton atras
