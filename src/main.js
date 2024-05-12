d005e174466670f9122cb03fb50aa898c3d21ec3
ignasimgol <ignasimgol@gmail.com>
2024-05-12 12:36:07 +0200

    back arrow wowrks

--- src/main.js
+++ src/main.js
@@ -1,50 +1,47 @@
 
-// Hamburger menu
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Hamburger menu
+  const nav = document.querySelector("#nav");
+  const abrir = document.querySelector("#abrir");
+  const cerrar = document.querySelector("#cerrar");
+
+  abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+  });
 
-cerrar.addEventListener("click", () => {
+  cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+  });
 
-// Contador
-const fechaObjetivo = moment('2038-01-19T03:14:07Z');
+  // Boton atrás
+  document.getElementById("btnBack").addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
 
-const intervalo = setInterval(() => {
-  
-  const ahora = moment();
+  // Contador
+  const fechaObjetivo = moment('2038-01-19T03:14:07Z');
 
-  
-  const diferencia = fechaObjetivo.diff(ahora);
+  const intervalo = setInterval(() => {
+    const ahora = moment();
+    const diferencia = fechaObjetivo.diff(ahora);
+    const dias = fechaObjetivo.diff(ahora, 'days');
+    const horas = fechaObjetivo.diff(ahora, 'hours') % 24;
+    const minutos = fechaObjetivo.diff(ahora, 'minutes') % 60;
+    const segundos = fechaObjetivo.diff(ahora, 'seconds') % 60;
 
+    document.getElementById('dias').innerHTML = `${dias} días`;
+    document.getElementById('horas').innerHTML = `${horas} horas`;
+    document.getElementById('minutos').innerHTML = `${minutos} minutos`;
+    document.getElementById('segundos').innerHTML = `${segundos} segundos`;
 
-  const dias = fechaObjetivo.diff(ahora, 'days');
-  const horas = fechaObjetivo.diff(ahora, 'hours') % 24;
-  const minutos = fechaObjetivo.diff(ahora, 'minutes') % 60;
-  const segundos = fechaObjetivo.diff(ahora, 'seconds') % 60;
-
-  
-  document.getElementById('dias').innerHTML = `${dias} días`;
-  document.getElementById('horas').innerHTML = `${horas} horas`;
-  document.getElementById('minutos').innerHTML = `${minutos} minutos`;
-  document.getElementById('segundos').innerHTML = `${segundos} segundos`;
-
-
-  if (diferencia < 0) {
-    clearInterval(intervalo);
-    document.getElementById('contador').innerHTML = 'TIME IS OVER';
-  }
-}, 1000);
+    if (diferencia < 0) {
+      clearInterval(intervalo);
+      document.getElementById('contador').innerHTML = 'TIME IS OVER';
+    }
+  }, 1000);
 
 
-
-// Boton atras
-document.getElementById("btnBack").addEventListener("click", function() {
-  history.back();
 });
 
