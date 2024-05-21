13d6029781c5fce4559c479d263ce97eb2e595c0
ignasimgol <ignasimgol@gmail.com>
2024-05-21 15:46:30 +0200

    back button

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Boton atrás
   document.getElementById("btnBack").addEventListener("click", function() {
-    window.location.href = "index.html";
+    window.location.href = "src/index.html";
   });
 
 
